22716a5f1654c1f23da6eda31826da6a129ce0e4
Mike Joyce <michael.joyce@target.com>
2017-12-11 06:35:07 -0600

    started attempt at parsing stat tables

--- src/scraper/players/playerSeasonStatScraper.js
+++ src/scraper/players/playerSeasonStatScraper.js
@@ -84,6 +84,67 @@ type PlayerSeasonStats = {
   defensiveStats?: DefensiveSeasonStat[]
 }
 
+/**
+ * 
+ * select all table.tablehead elements
+ * remove first and last tables (these are not stat tables)
+ * grab stat type from tr.stathead>td:first-child text() to get ${statType} Stats 
+ * grab all trs that are not stathead, colhead and total
+ * most columns are simple cells except for the team column, which you need 
+ * to grab the team id out of
+ * td li.team-name > a - get href and parse the team abbreviation out of
+ * url
+ * <td>
+  <ul class="game-schedule">
+    <li class="team-logo-small logo-nfl-small nfl-small-9">
+      <a href="http://www.espn.com/nfl/team/_/name/gb/green-bay-packers"></a>
+    </li>
+    <li class="team-name">
+      <a href="http://www.espn.com/nfl/team/_/name/gb/green-bay-packers">GB</a>
+    </li>
+  </ul>
+</td>
+ */
+
+const textToNumber = (value: string): number => {
+  if (isNaN(value)) {
+    return 0
+  }
+
+  return Number(value)
+}
+
+const parseTeamValue = (value: string): string => {
+  //   <td>
+  //   <ul class="game-schedule">
+  //     <li class="team-logo-small logo-nfl-small nfl-small-9">
+  //       <a href="http://www.espn.com/nfl/team/_/name/gb/green-bay-packers"></a>
+  //     </li>
+  //     <li class="team-name">
+  //       <a href="http://www.espn.com/nfl/team/_/name/gb/green-bay-packers">GB</a>
+  //     </li>
+  //   </ul>
+  // </td>
+  throw new Error('not implemented')
+}
+
+const passingColumnMapping = {
+  '0': 'season',
+  '1': 'team', //special
+  '2': 'gamesPlayed',
+  '3': 'completions',
+  '4': 'attempts',
+  '5': 'completionPct',
+  '6': 'yards',
+  '7': 'average',
+  '8': 'touchdowns',
+  '9': 'long',
+  '10': 'interceptions',
+  '11': 'fumbles',
+  '12': 'qbr',
+  '13': 'qbRating'
+}
+
 module.exports.scrapeSeasonStatsForPlayer = (
   id: number
 ): Promise<PlayerSeasonStats> => {
