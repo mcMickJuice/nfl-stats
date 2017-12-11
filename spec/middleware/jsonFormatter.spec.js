7705d8e3a995061eca45eac365679e39aec891c1
Mike Joyce <michael.joyce@target.com>
2017-12-10 20:06:32 -0600

    added eslintignore. updated spec files

--- spec/middleware/jsonFormatter.spec.js
+++ spec/middleware/jsonFormatter.spec.js
@@ -1,116 +1,118 @@
 const jsonFormatter = require('../../src/middleware/jsonFormatter')
 
-let middleware;
+let middleware
 beforeEach(() => {
-    middleware = jsonFormatter();
+  middleware = jsonFormatter()
 })
 
 const runMiddlewareWithContext = (middlewareFn, context) => {
-    middlewareFn = middlewareFn.bind(context);
-    const genFn = middlewareFn();
-    //run to completion
-    for (var i of genFn) { }
+  middlewareFn = middlewareFn.bind(context)
+  const genFn = middlewareFn()
+  //run to completion
+  for (var i of genFn) {
+  }
 }
 
 it('will not format calls not to /api', () => {
-    const originalBody = {
-        Hi: 'hey',
-        HowAreYa: 'oh hi'
-    }
-    const context = {
-        url: '',
-        body: originalBody
-    }
+  const originalBody = {
+    Hi: 'hey',
+    HowAreYa: 'oh hi'
+  }
+  const context = {
+    url: '',
+    body: originalBody
+  }
 
-    runMiddlewareWithContext(middleware, context)
+  runMiddlewareWithContext(middleware, context)
 
-    expect(context.body).toEqual(originalBody)
+  expect(context.body).toEqual(originalBody)
 })
 
 it('will not throw if body is undefined', () => {
-    const context = {
-        url: '/api/players',
-        body: undefined
-    }
+  const context = {
+    url: '/api/players',
+    body: undefined
+  }
 
-    expect(() => runMiddlewareWithContext(middleware, context)).not.toThrow()
+  expect(() => runMiddlewareWithContext(middleware, context)).not.toThrow()
 })
 
 it('format bodies of calls to /api', () => {
-    const originalBody = {
-        Hey: 'hi',
-        Hi: 'hey',
-        HowAreYa: 'howareya'
-    }
+  const originalBody = {
+    Hey: 'hi',
+    Hi: 'hey',
+    HowAreYa: 'howareya'
+  }
 
-    const originalBodyKeys = ['hey', 'hi', 'howAreYa'];
+  const originalBodyKeys = ['hey', 'hi', 'howAreYa']
 
-    const context = {
-        url: '/api/players',
-        body: originalBody
-    }
+  const context = {
+    url: '/api/players',
+    body: originalBody
+  }
 
-    runMiddlewareWithContext(middleware, context)
+  runMiddlewareWithContext(middleware, context)
 
-    expect(originalBody).not.toBe(context.body)
+  expect(originalBody).not.toBe(context.body)
 })
 
 it('camelCase keys of response body', () => {
-    const originalBody = {
-        Hey: 'hi',
-        Hi: 'hey',
-        HowAreYa: 'howareya'
-    }
+  const originalBody = {
+    Hey: 'hi',
+    Hi: 'hey',
+    HowAreYa: 'howareya'
+  }
 
-    const originalBodyKeys = ['hey', 'hi', 'howAreYa'];
+  const originalBodyKeys = ['hey', 'hi', 'howAreYa']
 
-    const context = {
-        url: '/api/players',
-        body: originalBody
-    }
+  const context = {
+    url: '/api/players',
+    body: originalBody
+  }
 
-    runMiddlewareWithContext(middleware, context)
+  runMiddlewareWithContext(middleware, context)
 
-    Object.keys(context.body).forEach(key => {
-        expect(originalBodyKeys.indexOf(key) > -1).toBe(true)
-    })
+  Object.keys(context.body).forEach(key => {
+    expect(originalBodyKeys.indexOf(key) > -1).toBe(true)
+  })
 })
 
 it('camelCase keys of response body if array', () => {
-    const originalBody = {
-        Hey: 'hi',
-        Hi: 'hey',
-        HowAreYa: 'howareya'
-    }
+  const originalBody = {
+    Hey: 'hi',
+    Hi: 'hey',
+    HowAreYa: 'howareya'
+  }
 
-    const originalBodyKeys = ['hey', 'hi', 'howAreYa'];
+  const originalBodyKeys = ['hey', 'hi', 'howAreYa']
 
-    const context = {
-        url: '/api/players',
-        body: [originalBody]
-    }
+  const context = {
+    url: '/api/players',
+    body: [originalBody]
+  }
 
-    runMiddlewareWithContext(middleware, context)
+  runMiddlewareWithContext(middleware, context)
 
-    context.body.forEach(obj => Object.keys(obj).forEach(key => {
-        expect(originalBodyKeys.indexOf(key) > -1).toBe(true)
-    }))
+  context.body.forEach(obj =>
+    Object.keys(obj).forEach(key => {
+      expect(originalBodyKeys.indexOf(key) > -1).toBe(true)
+    })
+  )
 })
 
 it('doesnt alter existing camelCased body', () => {
-    const originalBody = {
-        hey: 'hi',
-        hi: 'hey',
-        howAreYa: 'howareya'
-    }
+  const originalBody = {
+    hey: 'hi',
+    hi: 'hey',
+    howAreYa: 'howareya'
+  }
 
-    const context = {
-        url: '/api/players',
-        body: originalBody
-    }
+  const context = {
+    url: '/api/players',
+    body: originalBody
+  }
 
+  runMiddlewareWithContext(middleware, context)
 
-    runMiddlewareWithContext(middleware, context);
-
-    expect(originalBody).toEqual(context.body)
+  expect(originalBody).toEqual(context.body)
 })
--- spec/statMappers/statsToRows.spec.js
+++ spec/statMappers/statsToRows.spec.js
@@ -1,79 +1,82 @@
-const statsToRows  = require('../../src/statMappers/statsToRows')
+const statsToRows = require('../../src/statMappers/statsToRows')
 
 const find = (arr, findFunc) => {
-    return arr.filter(findFunc)[0];
+  return arr.filter(findFunc)[0]
 }
 
 const stats = [
-    {statType: 'SEASON', statValue: '2009'},
-    {statType: 'PASSING', statValue: '100'},
-    {statType: 'PT', statValue: '4'},
-    {statType: 'AWESOME', statValue: '2'},
+  { statType: 'SEASON', statValue: '2009' },
+  { statType: 'PASSING', statValue: '100' },
+  { statType: 'PT', statValue: '4' },
+  { statType: 'AWESOME', statValue: '2' }
 ]
 
 const statSet = [
-        {
-            season: '2009',
-            stats
-        }
-    ]
+  {
+    season: '2009',
+    stats
+  }
+]
 
-const statName = 'passing';
+const statName = 'passing'
 
 it('default filter filters out SEASON stattype', () => {
-    const functionToTest = statsToRows(statName);
+  const functionToTest = statsToRows(statName)
 
-    const result = functionToTest(statSet);
-    const processedStatSet = result[statName][0].stats;
-    
+  const result = functionToTest(statSet)
+  const processedStatSet = result[statName][0].stats
 
-    expect(Object.keys(processedStatSet).indexOf('season') === -1).toBe(true)
-});
+  expect(Object.keys(processedStatSet).indexOf('season') === -1).toBe(true)
+})
 
 it('filter provided filters out provided stats', () => {
-    const awesomeKeyword = 'awesome'
-    const filterOutAwesome = statSet => statSet.statType.toLowerCase() !== awesomeKeyword;
-    const functionToTest = statsToRows(statName, filterOutAwesome)
+  const awesomeKeyword = 'awesome'
+  const filterOutAwesome = statSet =>
+    statSet.statType.toLowerCase() !== awesomeKeyword
+  const functionToTest = statsToRows(statName, filterOutAwesome)
 
-    const result = functionToTest(statSet);
-    const processedStatSet = result[statName][0].stats;
+  const result = functionToTest(statSet)
+  const processedStatSet = result[statName][0].stats
 
-    expect(Object.keys(processedStatSet).indexOf(awesomeKeyword) === -1).toBe(true)
-});
+  expect(Object.keys(processedStatSet).indexOf(awesomeKeyword) === -1).toBe(
+    true
+  )
+})
 
 it('custom processor processes each statSet', () => {
-    const appendToItem = ' test';
-    const processor = statSet => Object.assign({}, statSet, {
-        statValue: `${statSet.statValue}${appendToItem}`
-    });
-
-    const functionToTest = statsToRows(statName, undefined, processor);
-
-    const result = functionToTest(statSet);
-    const processedStatSet = result[statName][0].stats;
-
-    Object.keys(processedStatSet).forEach(key => {
-        expect(processedStatSet[key].indexOf(appendToItem) > -1).toBe(true)
+  const appendToItem = ' test'
+  const processor = statSet =>
+    Object.assign({}, statSet, {
+      statValue: `${statSet.statValue}${appendToItem}`
     })
-});
+
+  const functionToTest = statsToRows(statName, undefined, processor)
+
+  const result = functionToTest(statSet)
+  const processedStatSet = result[statName][0].stats
+
+  Object.keys(processedStatSet).forEach(key => {
+    expect(processedStatSet[key].indexOf(appendToItem) > -1).toBe(true)
+  })
+})
 
 it('transform {statType, statValue} to key value', () => {
-    const functionToTest = statsToRows(statName);
+  const functionToTest = statsToRows(statName)
 
-    const result = functionToTest(statSet);
-    const processedStatSet = result[statName][0].stats;
+  const result = functionToTest(statSet)
+  const processedStatSet = result[statName][0].stats
 
-    Object.keys(processedStatSet).forEach(key => {
-        const item = find(stats, stat => stat.statType.toLowerCase() === key);
+  Object.keys(processedStatSet).forEach(key => {
+    const item = find(stats, stat => stat.statType.toLowerCase() === key)
 
-        expect(item.statValue).toEqual(processedStatSet[key])
-    })
+    expect(item.statValue).toEqual(processedStatSet[key])
+  })
 })
 
 it('returns object of key statName', () => {
-    const functionToTest = statsToRows(statName);
+  const functionToTest = statsToRows(statName)
 
-    const result = functionToTest(statSet);
+  const result = functionToTest(statSet)
 
-    expect(Object.keys(result)[0] === statName).toBe(true)
-});
+  expect(Object.keys(result)[0] === statName).toBe(true)
+})
